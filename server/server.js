70fbb20510dbb461689512ac9e29476f67838ba5
ch3coo2ca <ch3coo2ca@vivans.net>
2019-07-25 14:09:45 +0900

    FEAT : add server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const path = require('path'); 
+cosnt app = express(); 
+
+cosnt PORT = process.env.PORT || 4000; 
+
+app.use(express.static(path.join(__dirname,'..','public/'))); 
+
+app.get('/api/getData', (req,res) => {
+    res.send({data : 'test data '});
+}); 
+
+
+app.listen(PORT , () => {
+    console.log('check out the app at : http://localhost:'+PORT); 
+}); 
