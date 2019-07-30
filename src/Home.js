6b692c509716c2ff1a8e0503967f352c47d3237d
ch3coo2ca <wjddp3@naver.com>
2019-07-30 18:32:43 +0900

    FEAT : add loading spinner while fetching data from server

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,36 @@
 import React from "react";
 
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import AddCircle from "@material-ui/icons/AddCircle";
+import RemoveCircle from "@material-ui/icons/RemoveCircle";
+
 export const Home = () => {
-  return <div>HOME!!!</div>;
+  return (
+    <div>
+      HOME!!!
+      <IconButton
+        aria-label="delete"
+        onClick={() => console.log("button working ")}
+      >
+        <DeleteIcon size="small" />
+      </IconButton>
+      <IconButton
+        aria-label="delete"
+        onClick={() => console.log("button working ")}
+      >
+        <DeleteIcon size="medium" />
+      </IconButton>
+      <IconButton
+        aria-label="delete"
+        onClick={() => console.log("button working ")}
+      >
+        <DeleteIcon size="large" />
+      </IconButton>
+      <IconButton aria-label="add">
+        <AddCircle onClick={() => console.log("add")} />
+      </IconButton>
+    </div>
+  );
 };
--- src/RouterTest.js
+++ src/RouterTest.js
@@ -1,18 +1,39 @@
 import React from "react";
 import axios from "axios";
 
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 export class RouterTest extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      data: ""
+    };
+  }
   componentDidMount() {
     axios
       .get("/api/getData")
       .then(res => {
         console.log(res);
+
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+            data: res.data.data
+          });
+        }, 1000);
       })
       .catch(err => {
         console.log(err);
       });
   }
   render() {
-    return <div> a</div>;
+    const { loading, data } = this.state;
+
+    return (
+      <div>{loading ? <CircularProgress color="secondary" /> : data + ""}</div>
+    );
   }
 }
